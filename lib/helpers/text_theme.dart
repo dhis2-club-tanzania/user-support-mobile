0f160d91a7e5be5ad33dc64a52e4ff1d176a9eeb
wile44 <goodluckwileonline@gmail.com>
2023-10-20 12:31:29 +0300

    remove depricated code

--- lib/helpers/text_theme.dart
+++ lib/helpers/text_theme.dart
@@ -2,49 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 final TextTheme textTheme = TextTheme(
-  headline1: GoogleFonts.lato(
+  displayLarge: GoogleFonts.lato(
     fontSize: 97,
     fontWeight: FontWeight.w300,
   ),
-  headline2: GoogleFonts.lato(
+  displayMedium: GoogleFonts.lato(
     fontSize: 61,
     fontWeight: FontWeight.w300,
   ),
-  headline3: GoogleFonts.lato(fontSize: 48, fontWeight: FontWeight.w400),
-  headline4: GoogleFonts.lato(
+  displaySmall: GoogleFonts.lato(
     fontSize: 34,
     fontWeight: FontWeight.w400,
   ),
-  headline5: GoogleFonts.lato(fontSize: 24, fontWeight: FontWeight.w400),
-  headline6: GoogleFonts.lato(
+  headlineSmall: GoogleFonts.lato(fontSize: 24, fontWeight: FontWeight.w400),
+  titleLarge: GoogleFonts.lato(
     fontSize: 20,
     fontWeight: FontWeight.w500,
   ),
-  subtitle1: GoogleFonts.lato(
+  titleMedium: GoogleFonts.lato(
     fontSize: 16,
     fontWeight: FontWeight.w400,
   ),
-  subtitle2: GoogleFonts.lato(
+  titleSmall: GoogleFonts.lato(
     fontSize: 14,
     fontWeight: FontWeight.w500,
   ),
-  bodyText1: GoogleFonts.lato(
+  bodyLarge: GoogleFonts.lato(
     fontSize: 16,
     fontWeight: FontWeight.w400,
   ),
-  bodyText2: GoogleFonts.lato(
+  bodyMedium: GoogleFonts.lato(
     fontSize: 14,
     fontWeight: FontWeight.w400,
   ),
-  button: GoogleFonts.lato(
+  labelLarge: GoogleFonts.lato(
     fontSize: 14,
     fontWeight: FontWeight.w500,
   ),
-  caption: GoogleFonts.lato(
+  bodySmall: GoogleFonts.lato(
     fontSize: 12,
     fontWeight: FontWeight.w400,
   ),
-  overline: GoogleFonts.lato(
+  labelSmall: GoogleFonts.lato(
     fontSize: 10,
     fontWeight: FontWeight.w400,
   ),
